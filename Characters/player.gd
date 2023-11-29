1ca72016c558d51f8f6f0ad210eea8d4887f3fb8
Lizeth-V <Lizeth.valdovinosrodriguez01@student.csulb.edu>
2023-11-28 22:29:03 -0800

    player 1 update
    added more animations - player speed needs to be fixed

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,31 +1,38 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+const DOUBLE_JUMP_VELOCITY = -200.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 
-
+var can_double_jump = true
 
 func _physics_process(delta):
 	#Switching animations
 	if(velocity.x > 1 || velocity.x <-1):
-		animated_sprite = "run"
+		animated_sprite.animation = "run"
 	else:
-		animated_sprite = "idle"
+		animated_sprite.animation = "idle"
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		animated_sprite = "jump"
+		animated_sprite.animation = "jump"
 		
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = true
+		elif can_double_jump:
+			velocity.y = DOUBLE_JUMP_VELOCITY
+			can_double_jump = false
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -33,7 +40,7 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 12)
+		velocity.x = move_toward(velocity.x, 0, 10)
 
 	move_and_slide()
 	
