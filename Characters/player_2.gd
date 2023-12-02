b1ac749aea35785108497b2eddd930f82cf7da48
Lizeth-V <Lizeth.valdovinosrodriguez01@student.csulb.edu>
2023-12-02 01:03:40 -0800

    player 2 added!

---
+++ Characters/player_2.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
+const DOUBLE_JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite = $AnimatedSprite2D
+
+var can_double_jump = true
+
+func _physics_process(delta):
+	#Switching animations
+	if(velocity.x > 1 || velocity.x <-1):
+		animated_sprite.animation = "run"
+	else:
+		animated_sprite.animation = "idle"
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		animated_sprite.animation = "jump"
+		
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = true
+		elif can_double_jump:
+			velocity.y = DOUBLE_JUMP_VELOCITY
+			can_double_jump = false
+		
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 10)
+
+	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	animated_sprite.flip_h = isLeft
