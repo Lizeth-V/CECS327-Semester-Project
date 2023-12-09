4337809e9555916f152f2d14e0ffb5a58ad4c146
Lizeth-V <Lizeth.valdovinosrodriguez01@student.csulb.edu>
2023-12-09 12:42:45 -0800

    Multiplayer addition

--- Characters/player.gd
+++ Characters/player.gd
@@ -11,36 +11,41 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var can_double_jump = true
 
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
 func _physics_process(delta):
-	#Switching animations
-	if(velocity.x > 1 || velocity.x <-1):
-		animated_sprite.animation = "run"
-	else:
-		animated_sprite.animation = "idle"
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		animated_sprite.animation = "jump"
+	# Only the user can control this player - aka avoiding users ability to control other persons player
+	if is_multiplayer_authority():
+		#Switching animations
+		if(velocity.x > 1 || velocity.x <-1):
+			animated_sprite.animation = "run"
+		else:
+			animated_sprite.animation = "idle"
 		
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			animated_sprite.animation = "jump"
+			
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
-			can_double_jump = true
-		elif can_double_jump:
-			velocity.y = DOUBLE_JUMP_VELOCITY
-			can_double_jump = false
-		
+		# Handle Jump.
+		if Input.is_action_just_pressed("jump"):
+			if is_on_floor():
+				velocity.y = JUMP_VELOCITY
+				can_double_jump = true
+			elif can_double_jump:
+				velocity.y = DOUBLE_JUMP_VELOCITY
+				can_double_jump = false
+			
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, 10)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 10)
 
 	move_and_slide()
 	
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CECS327-Semester-Project
+
+This game was made specifically for the 327 Semester Project in order to demonstrate the uses of ditrubuted computing in multiplayer gaming 
  
